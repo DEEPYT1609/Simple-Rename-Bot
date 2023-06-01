44d0236f53dbceafe7c9a1ffff0ae92c350d7537
DEEPYT1609 <132783089+DEEPYT1609@users.noreply.github.com>
2023-06-01 11:07:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,15 @@
 from pyrogram import Client
 from config import *
 import os
+import logging
+import asyncio
+from aiohttp import web
+from main.web_support import web_server
+from main.web_support import ping_server
+
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+
 
 class Bot(Client):
     if not os.path.isdir(DOWNLOAD_LOCATION):
@@ -18,7 +27,15 @@ class Bot(Client):
         )
     async def start(self):
         await super().start()
-        me = await self.get_me()      
+        me = await self.get_me()
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        PORT = "8080"
+        await web.TCPSite(app, bind_address, PORT).start()
+        asyncio.create_task(ping_server())
+        logging.info("Web server started")
+        logging.info("Pinging server")
         print(f"{me.first_name} | @{me.username} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳...⚡️")
        
     async def stop(self, *args):
