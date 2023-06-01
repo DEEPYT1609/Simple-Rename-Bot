1bef201a73c5776c7ffe3654fdd68fbe070c52f6
DEEPYT1609 <132783089+DEEPYT1609@users.noreply.github.com>
2023-06-01 11:02:50 +0530

    Create web_support.py

---
+++ main/plugins/web_support.py
@@ -0,0 +1,33 @@
+from aiohttp import web
+import asyncio
+import logging
+import traceback
+import aiohttp
+
+routes = web.RouteTableDef()
+
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response("LazyDeveloper")
+
+
+async def web_server():
+    web_app = web.Application(client_max_size=30000000)
+    web_app.add_routes(routes)
+    return web_app
+
+
+async def ping_server():
+    sleep_time = PING_INTERVAL
+    while True:
+        await asyncio.sleep(sleep_time)
+        try:
+            async with aiohttp.ClientSession(
+                timeout=aiohttp.ClientTimeout(total=10)
+            ) as session:
+                async with session.get(f"http://0.0.0.0:8080") as resp:
+                    logging.info(f"Pinged server with response: {resp.status}")
+        except TimeoutError:
+            logging.warning("Couldn't connect to the site URL..!")
+        except Exception:
+            traceback.print_exc()
